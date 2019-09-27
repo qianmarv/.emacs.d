d65225474bedbbe122b31feb5031b45ab0a148b4
Marvin Qian <qianmarv@gmail.com>
2019-09-27 19:21:13 +0800

    1

--- org-clock-save.el
+++ org-clock-save.el
@@ -1,3 +1,2 @@
-;; org-clock-save.el - PVGN34378139A at <2019-09-25 Wed 17:24>
-(setq org-clock-stored-resume-clock '("/home/qianmarv/Org/GTD/Work.org" . 18965))
-(setq org-clock-stored-history '(("/home/qianmarv/Org/GTD/Work.org" . 41113) ("/home/qianmarv/Org/GTD/Work.org" . 193119) ("/home/qianmarv/Org/GTD/Work.org" . 33414) ("/home/qianmarv/Org/GTD/Work.org" . 144104) ("/home/qianmarv/Org/GTD/Work.org" . 18745)))
+;; org-clock-save.el - PVGN34378139A at <2019-09-25 Wed 19:46>
+(setq org-clock-stored-history '(("/home/qianmarv/Org/GTD/Work.org" . 41146) ("/home/qianmarv/Org/GTD/Work.org" . 193219) ("/home/qianmarv/Org/GTD/Work.org" . 33447) ("/home/qianmarv/Org/GTD/Work.org" . 18745) ("/home/qianmarv/Org/GTD/Work.org" . 144137)))
